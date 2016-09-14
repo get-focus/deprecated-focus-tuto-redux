fde3adcbca22cd58c4142432a1b2e3aa3da81bfa
Pierre Besson <pierre.besson7@gmail.com>
2016-09-14 13:54:27 +0200

    [modal] use components modal

--- src/app.js
+++ src/app.js
@@ -8,26 +8,15 @@ import React, {PropTypes} from 'react';
 import {compose} from 'redux';
 import {connect as connectToStore} from 'react-redux';
 import {connect as connectToMetadata} from 'focus-graph/behaviours/metadata';
-import DevTools from './containers/dev-tools';
-import Layout from 'focus-application/lib/layout'
-import LoadingBar from 'focus-application/lib/fetch'
-import ConfirmWrapper from 'focus-application/lib/confirm';
-import MessageCenter from 'focus-application/lib/messages';
-import AppHeader from 'focus-application/lib/header';
+
+import AppLayout from './containers/layout'
+
 // Ceci est un sélecteur de state, il sera localisé près de son reducer plus tard.
 const userSelector = state => ({...state.user});
 
-const StateDisplayer = connectToStore(s => s)(props => <pre><code>{JSON.stringify(props, null, 4)}</code></pre>)
 
 // On crée le composant Application
-const App = props =>
-  <Layout AppHeader={AppHeader} LoadingBar={LoadingBar} ConfirmWrapper={ConfirmWrapper} MessageCenter={MessageCenter}>
-    <DevTools />
-    <h1>Bienvenue dans ce superbe tutoriel dd{props.name} </h1>
-    {/* On récupère les définitions dans les props*/}
-    {props.children}
-    <StateDisplayer/ >
-  </Layout>;
+const App = props => <AppLayout {...props} />;
 
 App.defaultProps = {
   name: 'Without name maybe not...'
---
+++ src/containers/layout.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {connect as connectToStore} from 'react-redux';
+
+// focus-application
+import Layout from 'focus-application/lib/layout'
+import LoadingBar from 'focus-application/lib/fetch'
+import ConfirmWrapper from 'focus-application/lib/confirm';
+import MessageCenter from 'focus-application/lib/messages';
+import AppHeader from 'focus-application/lib/header';
+import DevTools from './dev-tools'
+// focus-components
+import ConfirmationPopin from 'focus-components/confirmation-popin';
+
+// Wrap application component with focus components
+const ConfirmComponent = props => <ConfirmWrapper {...props}  ConfirmationModal={ConfirmationPopin}/>
+
+
+const StateDisplayer = connectToStore(s => s)(props => <pre><code>{JSON.stringify(props, null, 4)}</code></pre>)
+
+
+//confirmation-popin
+
+function AppLayout(props){
+  return  (
+    <Layout AppHeader={AppHeader} LoadingBar={LoadingBar} ConfirmWrapper={ConfirmComponent} MessageCenter={MessageCenter}>
+    {props.hasDevtools  && <DevTools />}
+    <h1>Bienvenue dans ce superbe tutoriel dd{props.name} </h1>
+    {/* On récupère les définitions dans les props*/}
+    {props.children}
+    <StateDisplayer/ >
+  </Layout>);
+}
+
+AppLayout.defaultProps = {
+  hasDevtools: false
+}
+
+export default AppLayout
